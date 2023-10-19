7dd8e86ae9036e78646988b9b63f81bde32b8432
RobertoCarrera <robertocarrera93@gmail.com>
2023-10-19 14:18:50 +0200

    Basic structure
    Basic structure of the menu created.
    Responsiveness and styles left.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsrHomeComponent } from './components/usr-home/usr-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    pathMatch: 'full', 
+    redirectTo: '/inicio'
+  },
+  {
+    path: 'inicio',
+    component: UsrHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
