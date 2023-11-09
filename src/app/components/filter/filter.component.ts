4013a865810ca35a5ae0722ceb22376a8acb0b93
RobertoCarrera <robertocarrera93@gmail.com>
2023-11-10 00:53:45 +0100

    Could be final version
    Quite happy with this version. Only lacks the responsiveness

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { CharacteristicsService } from 'src/app/services/characteristics.service';
+import { LocationsService } from 'src/app/services/locations.service';
+import { TermsService } from 'src/app/services/terms.service';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
-export class FilterComponent {
+export class FilterComponent{
 
   showFilters: boolean = false;
+  @Input() showTerms: boolean = false;
+  @Input() showCategories: boolean = false;
+  @Input() showLocations: boolean = false;
+  @Input() showCharacteristics: boolean = false;
+  @Input() terms: any = [];
+  @Input() categories: any = [];
+  @Input() locations: any = [];
+  @Input() characteristics: any = [];
 
+  constructor(
+    private termsService: TermsService,
+    private categoriesService: CategoriesService,
+    private locationsService: LocationsService,
+    private characteristicsService: CharacteristicsService
+  ){}
   // Función para mostrar|esconder los diferentes filtros. Debería añadirla al 
   // conjunto de la app para que se esconda al clickar fuera.
   // Quizás, incluso, quitarlo cada vez que se marca algo y carga el contenido
--- src/app/components/usr-home/usr-home.component.ts
+++ src/app/components/usr-home/usr-home.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { CharacteristicsService } from 'src/app/services/characteristics.service';
+import { LocationsService } from 'src/app/services/locations.service';
 import { PropertiesService } from 'src/app/services/properties.service';
+import { TermsService } from 'src/app/services/terms.service';
 
 @Component({
   selector: 'app-usr-home',
@@ -9,8 +13,18 @@ import { PropertiesService } from 'src/app/services/properties.service';
 export class UsrHomeComponent {
 
   properties: any = [];
+  categories: any = [];
+  terms: any = [];
+  locations: any = [];
+  characteristics: any = [];
 
-  constructor(private propertiesService: PropertiesService){}
+  constructor(
+    private propertiesService: PropertiesService,
+    private termsService: TermsService,
+    private categoriesService: CategoriesService,
+    private locationsService: LocationsService,
+    private characteristicsService: CharacteristicsService,
+  ){}
 
   ngOnInit(): void{
 
@@ -18,6 +32,30 @@ export class UsrHomeComponent {
 
       this.properties = result;
       console.log(this.properties);
+    })
+
+    this.categoriesService.getAllCategories().subscribe(result => {
+
+      this.categories = result;
+      console.log(this.categories);
+    })
+
+    this.termsService.getAllTerms().subscribe(result => {
+
+      this.terms = result;
+      console.log(this.terms);
+    })
+
+    this.locationsService.getAllLocations().subscribe(result => {
+
+      this.locations = result;
+      console.log(this.terms);
+    })
+
+    this.characteristicsService.getAllCharacteristics().subscribe(result => {
+
+      this.characteristics = result;
+      console.log(this.characteristics);
     })
   }
 
